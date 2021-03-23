40e8fb23310dd0966958d941b55ec5167c86545f
KavyaGowda03 <81245271+KavyaGowda03@users.noreply.github.com>
2021-03-23 20:02:16 +0530

    Add files via upload

---
+++ Vehicle.java
@@ -0,0 +1,12 @@
+class Vehicle
+{
+  
+  Vehicle(String brand,int cost)
+  {
+   System.out.println("Brand:"+brand+"Cost:"+cost);
+  }
+   public static void main(String[] args)
+   {
+    Vehicle v1=new Vehicle("Audi",70000);
+   }
+}
